b9616cc8f4261a07cd0c9c08e752177b0c7d9773
Viktoryia Ventskute <ventskute.vika@gmail.com>
2020-10-25 12:33:33 +0300

    feat: add solution

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,14 +1,19 @@
+"use strict";
+
 module.exports = function check(str, bracketsConfig) {
-  let string = str;
-  for (let j = 0; j < 5; j++)
-    for (let i = 0; i < bracketsConfig.length; i++) {
-      const brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+  var string = str;
+
+  for (var j = 0; j < 5; j++) {
+    for (var i = 0; i < bracketsConfig.length; i++) {
+      var brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+
       while (true) {
         if (string.includes(brackets)) {
           string = string.replace(brackets, '');
         } else break;
       }
     }
+  }
 
   return string === '';
-}
+};
